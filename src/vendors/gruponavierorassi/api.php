c9031f2617a01198ee9c8aa66d7afb6e8232f870
zacketow <douglasgonzalez872@gmail.com>
2023-04-27 18:05:56 -0400

    V2.1

---
+++ src/vendors/gruponavierorassi/api.php
@@ -0,0 +1,28 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
+
+if($_GET and isset($_GET['tarifas'])){
+    $tarifas = "tarifas.json";
+    unlink($tarifas);
+    $file = fopen($tarifas, "w") or die("Unable to open file!");
+    $txt = $_POST['data'];
+    fwrite($file, $txt);
+    fclose($file);
+}
+if($_GET and isset($_GET['noticias'])){
+    $noticias = "noticias.json";
+    unlink($noticias);
+    $file = fopen($noticias, "w") or die("Unable to open file!");
+    $txt = $_POST['data'];
+    fwrite($file, $txt);
+    fclose($file);
+}
+
+?>
